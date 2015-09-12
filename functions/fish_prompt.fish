a178cd16bd1ca7e7682b42653702c75d6ab70436
Jiří Šebele <sindriava@gmail.com>
2015-09-12 12:12:25 +0200

    Include current git branch in `fish_prompt'

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,6 +1,27 @@
+function git_prompt
+  set -l branch (git branch --no-color 2> /dev/null | grep -e '\* ' | sed 's/^..\(.*\)/\1/')
+  set -l git_diff (git diff)
+
+  set_color purple
+  printf "%s" $branch
+  set_color normal
+
+  if test -n "$git_diff"
+    printf "*"
+  end
+
+  printf " "
+end
+
 function fish_prompt
   set_color yellow
   printf "λ "
   set_color normal
+
   printf "%s " (prompt_pwd)
+
+  set -l git_dir (git rev-parse --git-dir ^ /dev/null)
+  if [ $status = "0" ]
+    git_prompt
+  end
 end
